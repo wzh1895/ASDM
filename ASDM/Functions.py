f74f83116cafd8f9bc98b42ad00b7f65dc68b3d3
wzh <rutherford1895@gmail.com>
2023-01-23 19:00:45 +0000

    Reimplemented Conveyor to perfectly mimic Stella behaviour

--- ASDM/Functions.py
+++ ASDM/Functions.py
@@ -25,68 +25,61 @@ class Conveyor(object):
         self.length_time_units = length
         self.equation = eqn
         self.length_steps = None # to be decided at runtime
-        self.initial_total = None # to be decided when initialising stocks
-        self.conveyor = list() # pipe [new, ..., old]
-    
+        self.total = 0 # to be decided when initialising stocks
+        self.slats = list() # pipe [new, ..., old]
+        self.is_initialized = False
+        self.leaks = list()
+        self.leak_fraction = 0
+
     def initialize(self, length, value, leak_fraction=None):
-        self.initial_total = value
+        self.total = value
         self.length_steps = length
         if leak_fraction is None or leak_fraction == 0:
             for _ in range(self.length_steps):
-                self.conveyor.append(self.initial_total/self.length_steps)
+                self.slats.append(self.total/self.length_steps)
+                self.leaks.append(0)
         else:
-            # print('Conveyor Initial Total:', self.initial_total)
-            # print('Conveyor Leak fraction:', leak_fraction)
-            # length_steps * output + nleaks * leak = initial_total
-            # leak = output * (leak_fraction / length_in_steps)
-            # ==> length_steps * output + n(length) * (output * (leak_fraction/length_in_steps)) = initial_total
-            # print('Conveyor Length in steps:', self.length_steps)
+            self.leak_fraction = leak_fraction
             n_leak = 0
-            for i in range(1, self.length_steps+1):
-                n_leak += i
+            for i in range(self.length_steps):
+                n_leak += i+1
             # print('Conveyor N total leaks:', n_leak)
-            output = self.initial_total / (self.length_steps + n_leak * (leak_fraction / self.length_steps))
+            self.output = self.total / (self.length_steps + (n_leak * self.leak_fraction) / ((1-self.leak_fraction)*self.length_steps))
             # print('Conveyor Output:', output)
-            leak = output * (leak_fraction/self.length_steps)
+            leak = self.output * (self.leak_fraction/((1-self.leak_fraction)*self.length_steps))
             # print('Conveyor Leak:', leak)
             # generate slats
             for i in range(self.length_steps):
-                self.conveyor.append(output + (i+1)*leak)
-            self.conveyor.reverse()
+                self.slats.append(self.output + (i+1)*leak)
+                self.leaks.append(leak)
+            self.slats.reverse()
         # print('Conveyor Initialised:', self.conveyor, '\n')
+        self.is_initialized = True
 
     def level(self):
-        return sum(self.conveyor)
+        return self.total
 
-    def inflow(self, value):
-        self.conveyor = [value] + self.conveyor
+    # order of execution:
+    # 1 Leak from every slat
+    #   to do this we need to know the leak for every slat
+    # 2 Pop the last slat
+    # 3 Input as the first slat
 
+    def leak_linear(self):
+        for i in range(self.length_steps):
+            self.slats[i] = self.slats[i] - self.leaks[i]
+        
+        total_leaked = sum(self.leaks)
+        self.total -= total_leaked
+        return total_leaked
+    
     def outflow(self):
-        output = self.conveyor.pop()
+        output = self.slats.pop()
+        self.total -= output
+        self.leaks.pop()
         return output
 
-    def leak_linear(self, leak_fraction):
-        leaked = 0
-        for i in range(self.length_steps):
-            # print('Considering slat no.{}'.format(i+1))
-            # keep slats non-negative
-            if self.conveyor[i] > 0:
-                already_leak_fraction = leak_fraction*((i+1)/self.length_steps) # 1+1: position indicator starting from 1
-                # print('    Already leaked fraction:', already_leak_fraction)
-                remaining_fraction = 1-already_leak_fraction
-                original = self.conveyor[i] / remaining_fraction
-                i_to_be_leaked = original * leak_fraction / self.length_steps
-                # print('    Leaked no.{} to be leaked by {}:'.format(i+1, i_to_be_leaked))
-                if self.conveyor[i] - i_to_be_leaked < 0:
-                    i_to_be_leaked = self.conveyor[i]
-                    self.conveyor[i] = 0
-                else:
-                    self.conveyor[i] = self.conveyor[i] - i_to_be_leaked
-                leaked = leaked + i_to_be_leaked
-            else:
-                # print('    Slat no.{} is empty.'.format(i+1))
-                pass
-        # print('Leaked conveyor:', self.conveyor)
-        # print('Leaked:', leaked)
-        # print('\n')
-        return leaked
+    def inflow(self, value):
+        self.total += value
+        self.slats = [value] + self.slats
+        self.leaks = [value* self.leak_fraction/self.length_steps]+self.leaks
