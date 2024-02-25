21e0a6c6462508f6be92ad7bcaaaa3427bb47366
Wang Zhao <wzh1895@outlook.com>
2024-02-25 19:20:15 +0000

    cleansed test models

--- test_models/simple_care_home_model.py
+++
@@ -1,4 +0,0 @@
-from asdm.asdm import sdmodel
-model = sdmodel(from_xmile='TestModels/simple care home model.stmx')
-model.simulate(debug_against=False, verbose=False)
-result = model.export_simulation_result(format='df', to_csv=True)
